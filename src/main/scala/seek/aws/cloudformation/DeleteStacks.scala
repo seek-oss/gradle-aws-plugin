784fddcab84ac282526617d32e6899be56f7aaa0
Ashlin Eldridge <aeldridge@seek.com.au>
2017-12-13 09:01:19 +1100

    Added a safety switch to DeleteStacks

--- src/main/scala/seek/aws/cloudformation/DeleteStacks.scala
+++ src/main/scala/seek/aws/cloudformation/DeleteStacks.scala
@@ -17,26 +17,35 @@ class DeleteStacks extends AwsTask {
   private val nameMatching = lazyProperty[String]("nameMatching")
   def nameMatching(v: Any): Unit = nameMatching.set(v)
 
-  // TODO: implmenent safety on my default
-  private val safetyOn = lazyProperty[Boolean]("safetyOn")
+  private val safetyOn = lazyProperty[Boolean]("safetyOn", true)
   def safetyOn(v: Any): Unit = safetyOn.set(v)
+
+  private val safetyLimit = lazyProperty[Int]("safetyLimit", 3)
+  def safetyLimit(v: Any): Unit = safetyLimit.set(v)
 
   override def run: IO[Unit] =
     for {
       r  <- region
       c  <- IO.pure(AmazonCloudFormationClientBuilder.standard().withRegion(r).build())
       n  <- nameMatching.run
-      ds <- deleteStacks(n).run(c).runLog
-      _  <- waitForStacks(ds.toList).run(c)
+      so <- safetyOn.run
+      sl <- safetyLimit.run
+      ds <- deleteStacks(n, so, sl).run(c)
+      _  <- waitForStacks(ds).run(c)
     } yield ()
 
-  private def deleteStacks(nameMatching: String): Kleisli[Stream[IO, ?], AmazonCloudFormation, Stack] =
-    Kleisli[Stream[IO, ?], AmazonCloudFormation, Stack] { c =>
-      describeStacks.run(c).filter(_.getStackName.matches(nameMatching)).map { s =>
-        c.deleteStack(new DeleteStackRequest().withStackName(s.getStackName))
-        s
+  private def deleteStacks(nameMatching: String, safetyOn: Boolean, safetyLimit: Int): Kleisli[IO, AmazonCloudFormation, List[Stack]] =
+     Kleisli { c =>
+      describeStacks.run(c).filter(_.getStackName.matches(nameMatching)).runLog.flatMap { ss =>
+        if (safetyOn && ss.size > safetyLimit)
+          raiseError(s"Safety is on and the number of matching stacks (${ss.size}) exceeds the safety limit of ${safetyLimit}")
+        else
+          ss.foldLeft(IO.unit)((z, s) => z.flatMap(_ => deleteStack(s.getStackName).run(c))).map(_ => ss.toList)
       }
     }
+
+  private def deleteStack(name: String): Kleisli[IO, AmazonCloudFormation, Unit] =
+    Kleisli(c => IO(c.deleteStack(new DeleteStackRequest().withStackName(name))))
 
   private def waitForStacks(stacks: List[Stack]): Kleisli[IO, AmazonCloudFormation, Unit] =
     stacks match {
